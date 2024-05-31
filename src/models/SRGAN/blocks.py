61fd5298ddb425c47b7bf46cff8505ed7cb7ca45
Archisman <archismanninja@gmail.com>
2024-05-31 13:06:38 +0530

    added SRGAN blocks

--- src/models/SRGAN/blocks.py
+++ src/models/SRGAN/blocks.py
@@ -1,9 +1,9 @@
-import torch 
 import torch.nn as nn 
-import numpy as np 
 
 
 class Generator_Residual_Block(nn.Module):
+    """ Generator Residual Block 
+    Conv -> BN -> PReLU -> Conv -> BN -> Add"""
     def __init__(self, input_channels, output_channels, 
                  kernel_size, stride, padding):
         super(Generator_Residual_Block, self).__init__()
@@ -27,6 +27,8 @@ class Generator_Residual_Block(nn.Module):
     
 
 class DisCriminator_Residual_Block(nn.Module):
+    """ Discriminator Residual Block
+    Conv -> BN -> LeakyReLU"""
     def __init__(self, input_channels, output_channels,
                  kernel_size, stride, padding):
         super(DisCriminator_Residual_Block, self).__init__()
@@ -35,4 +37,29 @@ class DisCriminator_Residual_Block(nn.Module):
         self.activation = nn.LeakyReLU(0.2)
         self.bn = nn.BatchNorm2d(output_channels)
 
-    def forward(sel)
+    def forward(self, x):
+        output = self.conv(x)
+        output = self.bn(output)
+        output = self.activation(output)
+
+        return output
+    
+
+
+class Generator_Upsample_Block(nn.Module):
+    """Generator upsample block
+    Conv -> PixelShuffle -> PReLU"""
+    def __init__(self, input_channels, output_channels, 
+                 kernel_size, stride, padding, upsample_factor = 2):
+        super(Generator_Upsample_Block, self).__init__()
+        self.conv = nn.Conv2d(input_channels, output_channels, 
+                          kernel_size, stride, padding)
+        self.pixel_shuffle = nn.PixelShuffle(upsample_factor)
+        self.activation = nn.PReLU(output_channels)
+
+    def forward(self, x):
+        output = self.conv(x)
+        output = self.pixel_shuffle(output)
+        output = self.activation(output)
+
+        return output
